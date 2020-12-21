763f576e9aa66327a9506b985665c3480c824b5f
Corentin-Guerin <74669697+Corentin-Guerin@users.noreply.github.com>
2020-12-21 02:15:04 +0100

    css

--- jeu_js.js
+++ jeu_js.js
@@ -38,14 +38,16 @@ function clicmonstre(monstre,label) {	//Bouton d'attaque sur monstre avec en par
  /*--------------- Liste info  ------------------*/
  
  
- // clic perso 			> lance la fonction affichemenu sur le perso clicquer		>> affiche menu perso     	
+ // clic perso 			> lance la fonction affichemenu(persoclicquer) sur le perso clicquer		>> affiche menu du perso     	
 
- // clic menu attaque 	> lance la fonction clicattaque 	 > desactive autre choix 	>> active bouton monstre
- // clic menu defence	> desactive autre choix
- // clic menu special	> desactive autre choix
+ // clic menu attaque 	> lance la fonction clicattaque(persoNum) 		>> desactive autre choix 	>> active bouton monstre
+ // clic menu defence	> lance la fonction clicdefence(persoNum) 		>> desactive autre choix	>>Donne un bouclier au perso cliquer
+ // clic menu special	>> voir en fonction des joueurs
  
  
- // clic monstre 		> lance la fonction clicmonstre sur le monstre clicquer		>> reactive autre choix menu et desactive choix fait		>> masque menu perso		>>descative perso fait 		>> desactive bouton monstre >>check hp monstre
- // clic findetour		>lance attaque des monstre		>>reactive les perso
+ // clic monstre 		> lance la fonction clicmonstre(monstre,label) sur le monstre clicquer		>>Monstre perd de la vie 	>> reactive autre choix menu sauf specail si mana =0 et desactive choix fait		>> masque menu perso		>>descative perso fait 		>> desactive bouton monstre >>check hp monstre
+ // clic findetour		> lance la fonction findetour	>>lance attaque des monstre		>>reactive les perso		>>check hp perso
  
  
+ 
+ // clic menu special	j1 		 >> desactive autre choix  >> diminue mana 
